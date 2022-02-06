b512d5c2fc7a15a00975b086e9f72b0b135c97ef
Daniel Castillo <ec1335784@gmail.com>
2022-02-06 12:10:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/Ejercicio2.kt
@@ -0,0 +1,22 @@
+package com.example.helloworld
+
+import com.example.helloworld.HelloWorldKt.sumar
+
+/*fun main()
+{
+    val res1 = sumaArray(1,5, n3 = 6)
+}
+
+fun sumaArray(n1: Int?, n2: Int?, n3: Int?):Int{
+ var total = numeros
+    var numeros = intArrayOf(n1!! ,n2!! ,n3!! )
+
+    if(n1==null && n2==null && n3==null){
+
+    }
+
+
+
+
+
+}*/
